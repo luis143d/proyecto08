b9037168cecd172d8e66679b62c225e3cfee5540
luis143d <108910590+luis143d@users.noreply.github.com>
2023-05-21 19:13:09 -0500

    Add files via upload

---
+++ secion07/src/main/java/com/mycompany/secion07/Secion07.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.secion07;
+
+
+
+/**
+ *
+ * @author Murillo
+ */
+public class Secion07 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       double a = 2;
+       double b = 4;
+       double c = 8;
+       double d = 10;
+
+       double e = (a + (b / c)) / ((a / b) + c);
+       double f = (a+b+(a/b))/c ;
+       double g = (a-b)/(a+b);
+       double h = (a+(b/a+b+(b/c)))/(a+(b/(c+a)));
+       double i = (a+b+c)/(a+(b/c));
+       double j = (a+b+(c/(d*a)))/(a+b*(a/d));
+       double k = (a+(b/c)+d)/a;
+       double l=((a/b)-(b/c));
+       double m = a+(a+((a+b))/(c+d))/(a+(a/b));
+       double n = (a+b)+(c/d)+((b+c)/(b-c));
+       double o = (a+b)/(c+d);
+       double p = a+(b/c)+d;
+       double q = ((a+b)/c)+d;
+       double r = a+(b/(c+d));
+       double s = (a+(b/c)-d)/(a+(b/Math.pow(c, d)+(d/(a-(b/c)*d))));
+       System.out.println("El valor de e es: " + e);
+       System.out.println("El valor de f es: " + f);
+       System.out.println("El valor de g es: " + g);
+       System.out.println("El valor de h es: " + h);
+       System.out.println("El valor de i es: " + i);
+       System.out.println("El valor de j es: " + j);
+       System.out.println("El valor de k es: " + k);
+       System.out.println("El valor de l es: " + l);
+       System.out.println("El valor de m es: " + m);
+       System.out.println("El valor de n es: " + n);
+       System.out.println("El valor de o es: " + o);
+       System.out.println("El valor de p es: " + p);
+       System.out.println("El valor de q es: " + q);
+       System.out.println("El valor de r es: " + r);
+       System.out.println("El valor de s es: " + s);
+    }
+}
+
+
+
